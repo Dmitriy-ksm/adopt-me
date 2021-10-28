a7ca6ca98321e5f288ba54bbb29d95a632098805
dimadim4ik <dimadim4ik@gmail.com>
2021-10-28 16:07:55 +0300

    Intro Ends

---
+++ src/Details.js
@@ -0,0 +1,91 @@
+import { Component } from "react";
+//eslint-disable-next-line
+import { withRouter } from "react-router";
+import Carousel from "./Carousel";
+import ErrorBoundary from "./ErrorBoundary";
+import ThemeContext from "./ThemeContext";
+import Modal from "./Modal";
+
+class Details extends Component {
+  state = { loading: true, showModal: false };
+  // replaces the above thanks to @babel/plugin-proposal-class-properties & @babel/preset-env
+  //   constructor() {
+  //     super();
+
+  //     this.state = { loading: true };
+  //   }
+
+  async componentDidMount() {
+    const res = await fetch(
+      `http://pets-v2.dev-apis.com/pets?id=${this.props.match.params.id}`
+    );
+    const json = await res.json();
+    this.setState(
+      Object.assign(
+        {
+          loading: false,
+        },
+        json.pets[0]
+      )
+      // {
+      //     loading: false,
+      //     ...json.pets[0],
+      // }
+    );
+  }
+
+  toggleModal = () => this.setState({ showModal: !this.state.showModal });
+
+  adopt = () => (window.location = "http://bit.ly/pet-adopt");
+
+  render() {
+    if (this.state.loading) {
+      return <h2>loading ...</h2>;
+    }
+    const { animal, breed, city, state, description, name, images, showModal } =
+      this.state;
+
+    return (
+      <div className="details">
+        <Carousel images={images} />
+        <h1>{name}</h1>
+        <h2>
+          {animal} - {breed} - {city}, {state}
+        </h2>
+        <ThemeContext.Consumer>
+          {([theme]) => (
+            <button
+              onClick={this.toggleModal}
+              style={{ backgroundColor: theme }}
+            >
+              {" "}
+              Adopt {name}
+            </button>
+          )}
+        </ThemeContext.Consumer>
+        <p>{description}</p>
+        {showModal ? (
+          <Modal>
+            <div>
+              <h2>Whould you like to adopt {name}</h2>
+              <div className="buttons">
+                <button onClick={this.adopt}>Yes</button>
+                <button onClick={this.toggleModal}>No</button>
+              </div>
+            </div>
+          </Modal>
+        ) : null}
+      </div>
+    );
+  }
+}
+
+const DetailsWithRouter = withRouter(Details);
+
+export default function DetailsWithErrorBoundary() {
+  return (
+    <ErrorBoundary>
+      <DetailsWithRouter />
+    </ErrorBoundary>
+  );
+}
