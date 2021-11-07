22158c90085dda48d073f2747919e6a73abd32c4
dimadim4ik <dimadim4ik@gmail.com>
2021-11-07 15:23:26 +0200

    CodeSpliting, lazzy loading

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,11 @@
-import { useState } from "react";
+import { useState, lazy, Suspense } from "react";
 import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
-import SearchParams from "./Home/SearchParams";
-import Details from "./Details/Details";
+// import SearchParams from "./Home/SearchParams";
+// import Details from "./Details/Details";
 import ThemeContext from "./Shared/ThemeContext";
+
+const Details = lazy(() => import("./Details/Details"));
+const SearchParams = lazy(() => import("./Home/SearchParams"));
 
 const App = () => {
   const themeHook = useState("darkblue");
@@ -15,17 +18,19 @@ const App = () => {
             "url(http://pets-images.dev-apis.com/pets/wallpaperA.jpg)",
         }}
       >
-        <Router>
-          <header className="w-full mb-10 text-center p-7 bg-gradient-to-b from-indigo-dark via-pink-500 to-some-random-colors">
-            <Link to="/" className="text-6xl text-white hover:text-gray-200">
-              <h1>Adopt Me!</h1>
-            </Link>
-          </header>
-          <Routes>
-            <Route path="/details/:id" element={<Details />} />
-            <Route path="/" element={<SearchParams />} />
-          </Routes>
-        </Router>
+        <Suspense fallback={<h2>loading route ...</h2>}>
+          <Router>
+            <header className="w-full mb-10 text-center p-7 bg-gradient-to-b from-indigo-dark via-pink-500 to-some-random-colors">
+              <Link to="/" className="text-6xl text-white hover:text-gray-200">
+                <h1>Adopt Me!</h1>
+              </Link>
+            </header>
+            <Routes>
+              <Route path="/details/:id" element={<Details />} />
+              <Route path="/" element={<SearchParams />} />
+            </Routes>
+          </Router>
+        </Suspense>
       </div>
     </ThemeContext.Provider>
   );
